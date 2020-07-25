e8daff1699819fb7bbcf06264f8101630fb72f26
Kai Vandivier <kpvandivier@gmail.com>
2020-07-25 14:02:45 -0700

    Add admin field to user

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const userSchema = new Schema({
   email: { type: String /* ? */, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  admin: { type: Boolean, default: false },
 });
 
 module.exports = model("User", userSchema)
